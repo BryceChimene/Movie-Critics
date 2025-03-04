81f02c9fc214561e86efd6f6b7e3dc6d48e792d7
Bryce Chimene <brycechimene@yahoo.com>
2025-03-04 17:10:52 -0600

    Update UI, Established DB connection, Add new user popup

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 import express from 'express';
+import pg from 'pg';
 
-const port = 3000;
+// Environment Variable Import
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Declare Variables
+const serverPort = process.env.SERVER_PORT;
 const app = express();
+const dbPassword = process.env.DB_PASSWORD;
+const dbPort = process.env.DB_PORT;
 
+// Declare express
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(express.json());
 
-// Get Homescreen
-app.get('/', (req, res) => {
-    res.render('login/index');
-})
+// Connect to DB
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "critic",
+    password: dbPassword,
+    port: dbPort,
+});
+db.connect();
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+// Return users' name
+async function getUsers(){
+    try{
+        const res = await db.query("SELECT name FROM users");
+        return res.rows;
+    } catch (err){
+        console.error("ERROR executing query", err.stack);
+        return [];
+    }
+};
+
+// Get Homescreen
+app.get('/', async (req, res) => {
+    const users = await getUsers();
+    console.log("Users: ", users);
+    
+    res.render('login/index', {users: users});
+});
+
+app.listen(serverPort, () => {
+    console.log(`Server running on port ${serverPort}.`);
 });
